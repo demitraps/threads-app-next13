de5e7230022bab0843b0b63233a60f72fc31c297
demitra <hellodemitra@gmail.com>
2023-08-17 20:01:20 +0300

    Homepage navigation setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }

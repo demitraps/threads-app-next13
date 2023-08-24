bcca141e10910e268c3c7ad738d6949d4d4c0b62
demitra <hellodemitra@gmail.com>
2023-08-24 19:32:36 +0300

    Fetch posts/threads in homepage

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -37,3 +37,36 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize= 20) {
+  connectToDB();
+
+  //Calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  //Fetch the posts that have no parents (top-level threads)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
+  .sort({ createdAt: 'desc' })
+  .skip(skipAmount)
+  .limit(pageSize)
+  .populate({ path: 'author', model: User })
+  .populate({ 
+    path: 'children', 
+    populate: {
+    path: 'author',
+    model: User,
+    select: "_id name parentId image"
+  }
+})
+
+const totalPostsCount = await Thread.countDocuments({
+  parentId: { $in: [null, undefined] },
+});
+
+const posts = await postsQuery.exec();
+
+const isNext = totalPostsCount > skipAmount + posts.length;
+
+return { posts, isNext};
+  
+}

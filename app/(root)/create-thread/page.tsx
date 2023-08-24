b63ce532f87d1ea8391335ff56892c7d1cc6d4f9
demitra <hellodemitra@gmail.com>
2023-08-24 15:24:22 +0300

    Create user model

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,6 @@
+async function Page() {
+  return <h1 className="head-text">Create thread</h1>
+
+}
+
+export default Page;
---
+++ lib/mongoose.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let isConnected = false; //check connection status
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found.");
+  if (isConnected) return console.log("Already connected to MongoDB");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true;
+
+    console.log("Connected to MongoDB");
+
+  } catch (error) {
+    console.log(error);
+  }
+}

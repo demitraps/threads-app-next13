022ff5ad1571d01d95e0626ad2c09096070a6120
demitra <hellodemitra@gmail.com>
2023-08-24 20:53:13 +0300

    Create thread card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
@@ -20,7 +19,7 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id}
+                currentUserId={user?.id || ""}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -38,35 +38,72 @@ export async function createThread({
   }
 }
 
-export async function fetchPosts(pageNumber = 1, pageSize= 20) {
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   connectToDB();
 
   //Calculate the number of posts to skip
   const skipAmount = (pageNumber - 1) * pageSize;
 
   //Fetch the posts that have no parents (top-level threads)
-  const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
-  .sort({ createdAt: 'desc' })
-  .skip(skipAmount)
-  .limit(pageSize)
-  .populate({ path: 'author', model: User })
-  .populate({ 
-    path: 'children', 
-    populate: {
-    path: 'author',
-    model: User,
-    select: "_id name parentId image"
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+  try {
+    //TODO: Populate Community
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
   }
-})
-
-const totalPostsCount = await Thread.countDocuments({
-  parentId: { $in: [null, undefined] },
-});
-
-const posts = await postsQuery.exec();
-
-const isNext = totalPostsCount > skipAmount + posts.length;
-
-return { posts, isNext};
-  
 }

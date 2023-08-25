28e238167cbe86b54c8880dd10d617a7fed97e4b
demitra <hellodemitra@gmail.com>
2023-08-25 11:42:06 +0300

    Search page implementation

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,7 +1,53 @@
+import UserCard from "@/components/cards/UserCard";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+
+import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
 const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+const result = await fetchUsers({
+  userId: user.id,
+  searchString: '',
+  pageNumber: 1,
+  pageSize: 25
+});
+
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users found.</p>
+        ): (
+          <>
+          {result.users.map((person) => (
+            <UserCard
+            key={person.id}
+            id={person.id}
+            name={person.name}
+            username={person.username}
+            imgUrl={person.image}
+            personType='User'
+             />
+          ))}</>
+        )}
+
+      </div>
     </section>
   );
 };
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
   userId: string;
@@ -89,5 +90,56 @@ export async function fetchUserPosts(userId: string) {
   } catch (error: any) {
     throw new Error(`Failed to fetch user posts: ${error.message}`)
     
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId }
+    }
+
+    if (searchString.trim() !== '') {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+    .sort(sortOptions)
+    .skip(skipAmount)
+    .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext};
+
+  } catch (error: any) {
+    throw new Error(`Failed to fetch users: ${error.message}`)
   }
 }

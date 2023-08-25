4337657cad149367dbe7c8aeb2276515c6f1ba93
demitra <hellodemitra@gmail.com>
2023-08-25 10:45:04 +0300

    Profile page setup

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -37,7 +37,11 @@ const ThreadCard = ({
   isComment,
 }: Props) => {
   return (
-    <article className={`flex w-full flex-col rounded-xl ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"}`}>
+    <article
+      className={`flex w-full flex-col rounded-xl ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
@@ -61,7 +65,7 @@ const ThreadCard = ({
 
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-            <div className="mt-5 flex flex-col gap-3">
+            <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
               <div className="flex gap-3.5">
                 <Image
                   src="/assets/heart-gray.svg"
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -59,6 +60,34 @@ export async function fetchUser(userId: string) {
     
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}.`)
+    
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    //Find all threads authored by user with the given userId
+    const threads = await User.findOne({ id: userId })
+    .populate({
+      path: 'threads',
+      model: Thread,
+      populate: {
+        path: 'children',
+        model: Thread,
+        populate: {
+          path: 'author',
+          model: User,
+          select: 'name image id'
+        }
+      }
+    })
+
+    return threads;
+    
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`)
     
   }
 }

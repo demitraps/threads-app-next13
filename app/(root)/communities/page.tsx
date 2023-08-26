d4b395eaf6e5f69801e9702776578e7e1c374908
demitra <hellodemitra@gmail.com>
2023-08-26 16:05:12 +0300

    Search bar and Pagination implementation

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,33 +1,41 @@
-import CommunityCard from "@/components/cards/CommunityCard";
-import UserCard from "@/components/cards/UserCard";
-import { fetchCommunities } from "@/lib/actions/community.actions";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
-  const user = await currentUser();
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+import CommunityCard from "@/components/cards/CommunityCard";
 
+import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchCommunities({
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
   return (
-    <section>
-      <h1 className="head-text mb-10">Search</h1>
+    <>
+      <h1 className="head-text">Communities</h1>
 
-      {/* Search bar */}
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
 
-      <div className="mt-14 flex flex-col gap-9">
+      <section className="mt-9 flex flex-wrap gap-4">
         {result.communities.length === 0 ? (
-          <p className="no-result">No users found.</p>
+          <p className="no-result">No Results.</p>
         ) : (
           <>
             {result.communities.map((community) => (
@@ -43,9 +51,15 @@ const Page = async () => {
             ))}
           </>
         )}
-      </div>
-    </section>
+      </section>
+
+      <Pagination
+        path="communities"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
   );
-};
+}
 
 export default Page;

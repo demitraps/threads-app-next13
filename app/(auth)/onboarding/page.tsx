8bc898d68771ea653e8af4f671f7c818cd1445b8
demitra <hellodemitra@gmail.com>
2023-08-26 16:37:21 +0300

    Onboarding fix

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,29 +1,30 @@
-import AccountProfile from "@/components/forms/AccountProfile";
-import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
+
 async function Page() {
   const user = await currentUser();
-  if (!user) return null;
+  if (!user) return null; // to avoid typescript warnings
 
   const userInfo = await fetchUser(user.id);
-  if(userInfo?.onboarded) redirect("/");
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: user?.id,
+    id: user.id,
     objectId: userInfo?._id,
-    username: userInfo? userInfo?.username : user?.username,
-    name: userInfo? userInfo?.name : user?.firstName || "",
-    bio: userInfo? userInfo?.bio : "",
-    image: userInfo? userInfo?.image : user?.imageUrl,
-  }
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
 
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
       <p className="mt-3 text-base-regular text-light-2">
-        Complete your profile now to use Threads
+        Complete your profile now to use Threads!
       </p>
 
       <section className="mt-9 bg-dark-2 p-10">
